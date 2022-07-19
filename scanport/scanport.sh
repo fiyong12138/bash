8cbecddeaa2974f788926cfca1e1ab6545068419
fiyong12138 <62279417+fiyong12138@users.noreply.github.com>
2022-07-19 20:42:05 +0800

    Update scanport.sh

--- scanport/scanport.sh
+++ scanport/scanport.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
-ports=(5999,6000,6001,6005)
+ports=(80)
 for port in ${ports[@]};
 do
-    port_status=$(nmap hk.571314.xyz -p $port|sed -n "6p"|grep open|wc -l)
+    port_status=$(nmap baidu.com -p $port|sed -n "6p"|grep open|wc -l)
     if [ $port_status -lt 1 ]
-    then echo "hk-frp此端口".$port."已断开"|curl -k https://api.day.app/3ec29CNxxxxxxxxxxxxxxx/"hk-frp此端口$port已断开"
+    then echo "此端口".$port."已断开"|curl -k https://api.day.app/3ec29CNxxxxxxxxxxxxxxx/"此端口$port已断开"
     fi
 done
