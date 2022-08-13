6416e124f3e99b229ce3aad259d6485117df0b00
fiyong12138 <62279417+fiyong12138@users.noreply.github.com>
2022-08-13 15:04:16 +0800

    Update allkill.sh

--- allkill/allkill.sh
+++ allkill/allkill.sh
@@ -19,6 +19,3 @@ if [ $? -eq 0 ];then
 else
     echo "kill $input1 fail"
 fi
-————————————————
-版权声明：本文为CSDN博主「sunshine008」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/sun172270102/article/details/72801350
