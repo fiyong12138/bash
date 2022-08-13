86918be9ca8428fe552836e4cf3f209628dabd08
fiyong12138 <fiyong233@outlook.com>
2022-08-13 15:11:51 +0800

    Update allkill.sh

--- allkill/allkill.sh
+++ allkill/allkill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Input process name first"
+echo "输入要杀死的服务（例如frp）："
 read input1
 
 PID=$(ps -e|grep $input1|awk '{printf $1}')
@@ -19,6 +19,4 @@ if [ $? -eq 0 ];then
 else
     echo "kill $input1 fail"
 fi
-————————————————
-版权声明：本文为CSDN博主「sunshine008」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/sun172270102/article/details/72801350
+ ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill -9
